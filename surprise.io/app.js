d709d4238bd5bc1582d707c6cca9fec5a520728d
Jaap Mengers <jaap.mengers@gmail.com>
2014-01-07 23:05:56 +0100

    Verdere integratie met Angular. Template per view en eerste versie dashboard

--- surprise.io/app.js
+++ surprise.io/app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
   http = require('http'),
-  path = require('path');
+  path = require('path'),
+  us = require('underscore');
 
 var app = module.exports = express();
 var server = http.createServer(app);
@@ -10,9 +11,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 server.listen(3000);
 
+var setupTiles = function(){
+	var tiles = new Array();
+	for(var i =0; i < 12; i++){
+		tiles.push({number: i, opened: false});
+	}
+	return tiles;
+}
+var tiles = setupTiles();
+
 io.sockets.on('connection', function (socket) {
-  socket.emit('name', { name: 'Jaap' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+  var emit = function(eventName, data){
+  	socket.broadcast.emit(eventName, data);
+  	socket.emit(eventName, data);
+  }
+
+  socket.on('gotoQuestion', function (data) {
+    emit('gotoQuestion', data);
+  });
+
+  socket.on('setCorrect', function(data){
+  	var item = us.find(tiles, function(it){
+  		return it.number == data
+  	});
+  	if(item){
+  		item.opened = true;
+  	}
+  });
+
+  socket.on('getTiles', function(){
+  	console.log(tiles);
+		emit('send:tiles', tiles);
   });
 });
--- surprise.io/public/app.js
+++ surprise.io/public/app.js
@@ -1,1 +1,25 @@
-var app = angular.module('myApp', ['myApp.controllers']);
+var app = angular.module('quizApp', ['ngRoute', 'quizControllers']);
+
+app.config(['$routeProvider', 
+	function($routeProvider){
+		$routeProvider.
+			when('/hulplijn', {
+				templateUrl: 'partials/supportVote.html',
+				controller: 'SupportVoteCtrl'
+			}).
+			when('/board', {
+				templateUrl: 'partials/board.html',
+				controller: 'BoardCtrl'
+			}).
+			when('/question/:questionId/:tileId', {
+				templateUrl: 'partials/question.html',
+				controller: 'QuestionCtrl'
+			}).
+			when('/controller', {
+				templateUrl: 'partials/controller.html',
+				controller: 'ControllerCtrl'
+			}).
+			otherwise({
+				redirectTo: '/hulplijn'
+			});
+	}]);
