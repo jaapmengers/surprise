7c02ffb78de49da7f72aa0f503acc503fd28faa2
Jaap Mengers <jaap.mengers@gmail.com>
2014-01-13 22:43:57 +0100

    Extra dirs toegevoegd

---
+++ surprise.io/public/supportvote/supportvote.js
@@ -0,0 +1,62 @@
+'use strict';
+
+/* Controllers */
+var app = angular.module('quizApp', ['ngRoute', 'quizControllers']);
+app.config(['$routeProvider', 
+  function($routeProvider){
+    $routeProvider.
+      when('/leeg', {
+        templateUrl: 'partials/leeg.html',
+        controller: 'SupportVoteCtrl'
+      }).
+      when('/question/:questionNr', {
+        templateUrl: 'partials/question.html',
+        controller: 'QuestionCtrl'
+      }).
+      otherwise({
+        redirectTo: '/leeg'
+      });
+  }]);
+
+var quizControllers = angular.module('quizControllers', []);
+var didBind = false;
+
+quizControllers.controller('SupportVoteCtrl', ['$scope', '$location', 'socket', function ($scope, $location, socket) {
+
+  $scope.$on('$destroy', function (event) {
+    socket.removeAllListeners();
+  });
+
+  socket.on('receive:showQuestion', function(data){
+    console.log('receive:showQuestion', data);
+    $location.path('/question/' + data);
+    $scope.$$phase || $scope.$apply();
+  });
+  
+}]);
+
+quizControllers.controller('QuestionCtrl', ['$scope', '$location', '$routeParams', 'socket', function ($scope, $location, $routeParams, socket) {
+  var nr = $routeParams.questionNr;
+  $scope.question = _.find(questions, function(it){
+    return it.number == nr;
+  });
+
+  var roundEnded = function(){
+    $location.path('/leeg');
+    $scope.$$phase || $scope.$apply();
+  }
+
+  $scope.vote = function(answer){
+    console.log(answer.number);
+    socket.emit('request:doVote', answer.number);
+    $location.path('/leeg');
+    $scope.$$phase || $scope.$apply();
+  };
+
+  socket.on('receive:roundEnded', roundEnded);
+
+  $scope.$on('$destroy', function (event) {
+    socket.removeAllListeners();
+  });
+}]);  
+
